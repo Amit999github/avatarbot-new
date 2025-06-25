9899d48a268d146c34722c069502bdfa15dd5a8e
Amit Athiya <145979041+Amit999github@users.noreply.github.com>
2025-06-25 18:26:32 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,6 @@
 
 module.exports.isLoggedIn = (req ,res ,next) =>{
     if(!req.session.user){
-        req.flash('error', 'You must be signed in.');
         return res.redirect('/auth/signin');
     }
     next();
